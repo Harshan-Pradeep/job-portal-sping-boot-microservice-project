b94dc5904218cb629fd49018290b25808d78e320
Harshan Pradeep <89571794+Harshan-Pradeep@users.noreply.github.com>
2024-11-17 14:59:23 +0530

    Removed seperate applications instead of wrap with main project.

--- joblistings/src/main/java/com/jobportal/joblistings/repository/ListingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jobportal.joblistings.repository;
-
-import com.jobportal.joblistings.entity.Listing;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ListingRepository extends JpaRepository<Listing, Long> {
-
-}
